4beb935187ce18a6262054bca7e992818f8cc623
Confidence Yobo <confiyobo@gmail.com>
2022-04-20 13:03:27 +0100

    Add blog link to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -162,10 +162,26 @@ export default function Navbar (props: any) {
         {/* </Link> */}
         {/* // <Link to='/rider'  > */}
         <a
+          href="https://everestxhq.medium.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={
+            localStorage.getItem("color") + "" === "dark"
+              ? { color: "#FFFFFF" }
+              : { color: "#727272" }
+          }
+          className={
+            localStorage.getItem("tab") + "" === ""
+              ? "font-CircularStd-Regular border-b border-white cursor-pointer mx-6 pb-2 "
+              : "font-CircularStd-Regular cursor-pointer mx-6 pb-2 "
+          }
+        >
+          Blog
+        </a>
+        <a
           href="/whitepaper.pdf"
           target="_blank"
           rel="noopener noreferrer"
-          onClick={() => ClickHandler("rider")}
           style={
             localStorage.getItem("color") + "" === "dark"
               ? { color: "#FFFFFF" }
